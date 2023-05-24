e9e7244b8caa67279ff2801782d2acbd3d1617a8
Emmyvitamin <132033178+Emmyvitamin@users.noreply.github.com>
2023-05-24 02:21:26 +0100

    Add files via upload

---
+++ restaurant website/script.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', () => {
+  let menu = document.querySelector('#menu');
+  let navbar = document.querySelector('.navbar');
+
+  menu.onclick = () => {
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+  }
+});
+
+
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 7500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+ loop:true,
+});
